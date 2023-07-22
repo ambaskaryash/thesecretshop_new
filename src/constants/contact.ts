6ddbe0e4e49ab4ccbdafd4a685baca5bb0fc2a34
Yash Ambaskar <yash.ambaskar@corecotechnologies.com>
2023-07-22 14:00:56 +0000

    minors

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -15,27 +15,33 @@ export const contactInfos: Readonly<ContactInfo[]> = [
     iconName: 'MapPinIcon'
   },
   {
-    href: 'mailto:site@buburnusantara.com',
+    href: 'mailto:admin@example.com',
     style: 'hover:bg-blue-400 focus-visible:bg-blue-400',
     title: 'Email',
     iconName: 'AtIcon'
   },
   {
-    href: 'tel:+6285692807048',
+    href: 'tel:+919284280275',
     style: 'hover:bg-green-400 focus-visible:bg-green-400',
     title: 'Phone',
     iconName: 'PhoneIcon'
   },
   {
-    href: 'https://www.instagram.com/buburnusantara.id',
+    href: 'https://www.instagram.com/thesecretshopnagpur/',
     style: 'hover:bg-rose-400 focus-visible:bg-rose-400',
     title: 'Instagram',
     iconName: 'InstagramIcon'
   },
   {
-    href: 'https://wa.me/6285692807048',
+    href: 'https://wa.me/9284280275',
     style: 'hover:bg-green-400 focus-visible:bg-green-400',
     title: 'WhatsApp',
     iconName: 'WhatsAppIcon'
+  },
+  {
+    href: 'https://www.facebook.com/thesecretshopnagpur',
+    style: 'hover:bg-green-400 focus-visible:bg-green-400',
+    title: 'Facebook',
+    iconName: 'FacebookIcon'
   }
 ];
